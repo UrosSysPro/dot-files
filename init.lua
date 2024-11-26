9d7a4c0ce2bb461ac6f25d0d2d993190b6328f2a
uros <karaleicu@gmail.com>
2024-11-26 21:50:09 +0100

    first commit, lazy, metals, telescope

---
+++ init.lua
@@ -0,0 +1,19 @@
+require('config.lazy')
+
+print("hello")
+
+vim.cmd("colorscheme habamax")
+
+vim.g.counter=0
+vim.keymap.set('n','b',function() 
+	local counter=vim.g.counter
+	counter=counter+1
+	vim.g.counter=counter
+	vim.print(counter)
+end)
+
+vim.keymap.set('i','zz',function() 
+	vim.print('zz')
+end)
+
+
