261ef8a27a0194f4ab2c47d667b55930ebce4b96
uros <karaleicu@gmail.com>
2025-03-05 01:02:49 +0100

    radi metals i lua lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,12 +6,13 @@ return {
 			ft = "lua",
 			opts = {
 				library = {
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } }			
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } }
 				}
 			}
 		},
 		config = function()
 			require("lspconfig").lua_ls.setup {}
+			require("lspconfig").metals.setup{}
 		end
 	}
 }
