52030de137fc6f78c738dbc46707cfb9ae9f8389
uros <karaleicu@gmail.com>
2025-03-27 22:53:43 +0100

    radi ctrl d i ctrl space

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,13 +2,13 @@ return {
 
 	enabled=false,
 	"nothing/nothing",
-  dependencies = { },
-  ft = { "txt" },
-  opts = function()
-    vim.print("hello from opts")
-    return {}
-  end,
-  config = function(self, config)
-    vim.print("hello from config")
-  end
+	dependencies = { },
+	ft = { "txt" },
+	opts = function()
+		vim.print("hello from opts")
+		return {}
+	end,
+	config = function(self, config)
+		vim.print("hello from config")
+	end
 }
