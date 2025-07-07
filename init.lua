2bc12c2cb2a8a0ac935e44b4dff612fc8b117f84
Uros Karaleic <karaleicuu@gmail.com>
2025-07-07 20:25:14 +0200

    nis

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
 require('config.lazy')
+
 vim.opt.number=true
 vim.opt.tabstop=4
 vim.opt.shiftwidth=4
 --vim.opt.relativenumber=true
 vim.cmd("colorscheme monokai-pro")
-vim.keymap.set('n','<C-b>', function()
-	vim.cmd('Neotree')
-end)
-vim.keymap.set('n','<C-t>', function()
-	vim.cmd('Telescope')
-end)
-vim.keymap.set('n','<C-q>','<cmd>wqall')
+--neo tree and telescope
+vim.keymap.set('n','<leader>b','<cmd>Neotree<cr>')
+vim.keymap.set('n','<leader>t','<cmd>Telescope<cr>')
+--ctrl q and ctrl d
+vim.keymap.set('n','<C-q>','<cmd>wqall<cr>')
 vim.keymap.set('n','<C-d>','yyp')
+--omnicomplete
 vim.keymap.set('n','<C-Space>', '<C-x><C-o>')
-vim.keymap.set('n','<C-20>', '<C-x><C-o>')
