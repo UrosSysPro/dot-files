5d233356b94cd568333c2c303279082990bb721f
Uros Karaleic <karaleicuu@gmail.com>
2025-07-07 21:31:52 +0200

    nz,

--- init.lua
+++ init.lua
@@ -8,6 +8,11 @@ vim.cmd("colorscheme monokai-pro")
 --neo tree and telescope
 vim.keymap.set('n','<leader>b','<cmd>Neotree<cr>')
 vim.keymap.set('n','<leader>t','<cmd>Telescope<cr>')
+--split windows
+vim.keymap.set('n','<leader>j','<cmd>vsplit<cr>')
+vim.keymap.set('n','<leader>l','<cmd>split<cr>')
+--resize windows
+--vim.keymap.set('n','<S-->','<cmd>split<cr>')
 --ctrl q and ctrl d
 vim.keymap.set('n','<C-q>','<cmd>wqall<cr>')
 vim.keymap.set('n','<C-d>','yyp')
