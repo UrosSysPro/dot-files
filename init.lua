8e2a6082446ccb45c2b8042e09136da2789967bc
Uros Karaleic <karaleicuu@gmail.com>
2026-02-03 11:13:47 +0100

    remove lsp and metals from nvim config for now

--- init.lua
+++ init.lua
@@ -3,18 +3,25 @@ require('config.lazy')
 vim.opt.number=true
 vim.opt.tabstop=4
 vim.opt.shiftwidth=4
---vim.opt.relativenumber=true
 vim.cmd("colorscheme monokai-pro")
+
 --neo tree and telescope
 vim.keymap.set('n','<leader>b','<cmd>Neotree<cr>')
 vim.keymap.set('n','<leader>t','<cmd>Telescope<cr>')
+
 --split windows
-vim.keymap.set('n','<leader>j','<cmd>vsplit<cr>')
-vim.keymap.set('n','<leader>l','<cmd>split<cr>')
---resize windows
---vim.keymap.set('n','<S-->','<cmd>split<cr>')
+vim.keymap.set('n','<leader>v','<cmd>vsplit<cr>')
+vim.keymap.set('n','<leader>s','<cmd>split<cr>')
+
+--select window
+vim.keymap.set('n','<leader>h','<C-w>h')
+vim.keymap.set('n','<leader>j','<C-w>j')
+vim.keymap.set('n','<leader>k','<C-w>k')
+vim.keymap.set('n','<leader>l','<C-w>l')
+
 --ctrl q and ctrl d
 vim.keymap.set('n','<C-q>','<cmd>wqall<cr>')
 vim.keymap.set('n','<C-d>','yyp')
+
 --omnicomplete
 vim.keymap.set('n','<C-Space>', '<C-x><C-o>')
